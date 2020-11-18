264cd58fa4e7f549ad6f9239d3d45fc6cb7180c8
Bolvvv <605725874@qq.com>
2020-11-18 21:33:05 +0800

    完成forward中的encode部分

--- my_model.py
+++ my_model.py
@@ -1,11 +1,40 @@
 import torch
 import torch.nn as nn
+from utils import pad_batch_word_to_id
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
 class my_model(nn.Module):
     
-    def __init__(self, vocab_size, emb_size, hidden_size, padding_idx):
+    def __init__(self, q_map, a_map, emb_size, hidden_size, padding_idx):
         super(my_model, self).__init__()
-        self.emb = nn.Embedding(vocab_size, emb_size, padding_idx=padding_idx)#设置词嵌入，并设置padding为0
+        self.q_emb = nn.Embedding(len(q_map), emb_size, padding_idx=q_map['<pad>'])#设置词嵌入，并设置padding为0
+        self.a_emb = nn.Embedding(len(a_map), emb_size, padding_idx=a_map['<pad>'])
+        self.encode = nn.LSTM(emb_size, hidden_size, bidirectional=True, batch_first=True)
+        self.decode = nn.LSTMCell(hidden_size+emb_size, hidden_size)#这一步是将目标词汇和上一步的decode输出词汇进行链接
+        self.doubleH2oneH = nn.Linear(2*hidden_size, hidden_size)#将双向LSTM的输出值h变换为单hidden
+        self.doubleC2oneC = nn.Linear(2*hidden_size, hidden_size)#将双向LSTM的输出值c变换为单hidden
+        self.att_projection = nn.Linear(2*hidden_size, hidden_size)#将encode和decode的h输出组合生成attention
+        self.softmax = nn.Softmax()
+        self.combined_output_projection = nn.Linear(3*hidden_size, hidden_size)#将从注意力生成的encode输出与当前decode的输出组合在一起生成新的hidden
+        self.target_answer_projection = nn.Linear(hidden_size, len(a_map))#将decode的输出hidden转换为answer_map的大小，再使用softmax求出需要用到的词
+
+    def forward(self, question2list_id, answer2list_id):
+        q_pad, a_pad, q_len, a_len = pad_batch_word_to_id(question2list_id, answer2list_id, self.pad_token, self.q_map, self.a_map)
+        out_put, decode_init_state = self.forward_step(q_pad, q_len)#准备好解码需要用到的值。其中out_put还需要经过attention层，decode_init_state包含了hidden和cell
+
+
+    def forward_step(self, q_pad, q_len):
+        X = self.q_emb(q_pad)#在此步进行词嵌入，X的形式为(batch_size, question_len, emb_size)
+        #参考:https://blog.csdn.net/lssc4205/article/details/79474735
+        X = pack_padded_sequence(X, q_len, batch_first=True)#对嵌入后的矩阵进行压缩，告诉LSTM有些pad值是不需要的
+        out_put, (h_n, c_n) = self.encode(X)#dim(h_n) = dim(c_n) = (2, batch_size, hidden_size)
+        out_put_padded, _ = pad_packed_sequence(out_put, batch_first=True)#恢复回来out_put:(batch_size, question_len, num_directions * hidden_size)
+
+        init_decoder_hidden = self.doubleH2oneH(torch.cat((h_n[0], h_n[1]), dim=1))#易知dim(h_n)=(2, b, h),则dim(h_n[0])=(b,h),设置dim=1则表明在h纬度上拼接
+        init_decoder_cell = self.doubleC2oneC(torch.cat((c_n[0], c_n[1]), dim=1))
         
+        dec_init_state = (init_decoder_hidden, init_decoder_cell)
+        return out_put_padded, dec_init_state
+
+
         
-    
--- utils.py
+++ utils.py
@@ -13,8 +13,8 @@ a_save_path = "answer_trans.csv"
 def data2list(q_file_path, a_file_path):
     """
     根据数据集的特征，将数据解析为两个list列表，形式均为list[list[]]，按顺序一一对应问与答，具体形式如下
-    question: [['头痛', '恶心', '肌肉', '痛', '关节痛', '颈部', '淋巴结', '疼痛', '怎么回事', '啊'], ...]
-    answer: [['月经', '延迟', '十四天', '而且', '伴随', '恶心', '，', '头痛', '，', '乏力', '的', '现象', '，', '那么', '考虑', '怀孕', '的', '概率', '是', '非常', '大', '的'],...]
+    q_list: [['头痛', '恶心', '肌肉', '痛', '关节痛', '颈部', '淋巴结', '疼痛', '怎么回事', '啊'], ...]
+    a_list: [['月经', '延迟', '十四天', '而且', '伴随', '恶心', '，', '头痛', '，', '乏力', '的', '现象', '，', '那么', '考虑', '怀孕', '的', '概率', '是', '非常', '大', '的'],...]
     """
     q_file = open(q_file_path)
     a_file = open(a_file_path)
@@ -60,3 +60,49 @@ def step_build_map(lists):
             if e not in maps:
                 maps[e] = len(maps)
     return maps
+
+def pad_batch_word_to_id(q_list, a_list, pad_token, q_map, a_map):
+    """
+    将形如list[list[]]的q和a(其中值为中文)转换为list[list[]]的q和a(其中值为字典中的对应数字)，
+    且从高到低进行排序，再进行数据填充
+    """
+    #将中文转换为对应map的id
+    q_trans = word2id(q_list, q_map)
+    a_trans = word2id(a_list, a_map)
+    
+    #对列表进行填充，并存储其排序后的每个句子的长度
+    q_trans_pad, q_len = pad_list(q_trans, pad_token)
+    a_trans_pad, a_len = pad_list(a_trans, pad_token)
+    return q_trans_pad, a_trans_pad, q_len, a_len
+
+def word2id(lists, word_map):
+    """
+    将形如list[list[]]其中的值为中文转换为对应map的id，最终返回List[listp[]]，其中值为数字id
+    """
+    for l in lists:
+        for i in range(len(l)):
+            l[i] = word_map[l[i]]
+    return l
+
+def pad_list(lists, pad_token):
+    """
+    将形如list[list[]]按照里面list的长度从高到底排列，并进行填充使其长度一致，最后返回形如list[listp[]]，
+    其中值为数字，以及该列表的原始长度
+    """
+    sents_padded = []
+    lists.sort(key=lambda l: len(l),reverse=True)
+
+    lists_len = [len(s) for s in lists]#存储原始长度
+
+    max_length = len(lists[0])
+    for l in lists:
+        while len(l) != max_length:
+            l.append(pad_token)
+    sents_padded = lists
+    return sents_padded, lists_len
+
+# q_list, a_list = data2list(q_file_path, a_file_path)
+# q2id_map, a2id_map = build_map(q_list, a_list)
+# pad_batch_word_to_id(q_list, a_list, 0,q2id_map, a2id_map)
+
+    
